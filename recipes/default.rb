e29aed8e2cfdf41c26a51e70c2a5c44f1cda5c08
rreilly-edr <rreilly@edrnet.com>
2023-03-08 09:29:23 -0500

    init

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
+chef_client_updater 'Install latest Chef Infra Client 16.x' do
+  version '16'
+  post_install_action 'kill'
+end
